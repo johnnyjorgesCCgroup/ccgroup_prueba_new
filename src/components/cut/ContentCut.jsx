7f46b94457e339f368a9b2be32701e2612c389b1
Julio Caqui <juliojesuscaqui@gmail.com>
2024-02-13 14:44:45 -0500

    13022024

--- src/components/cut/ContentCut.jsx
+++ src/components/cut/ContentCut.jsx
@@ -78,8 +78,20 @@ const ContentCut = () => {
       return;
     }
 
-    const formData = new FormData();
-    formData.append('file', selectedFile);
+    const reader = new FileReader();
+
+    
+    reader.onload = (event) => {
+      const data = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+      console.log(jsonData);
+      // Enviar los datos a la API
+    //  sendDataToAPI(jsonData);
+    };
 
   };
 
